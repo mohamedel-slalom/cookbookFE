32668a0165154604919ad081ddf09c0035cc6b2d
Mohamed Elnoamany <melnoamany@aegpresents.com>
2026-03-29 13:15:04 -0500

    cleaning up linting issues

--- CookbookFE/src/components/RecipeCard.jsx
+++ CookbookFE/src/components/RecipeCard.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 function RecipeCard({ recipe }) {
   return (
     <article className="recipeCard">
@@ -12,6 +14,16 @@ function RecipeCard({ recipe }) {
       </div>
     </article>
   )
+}
+
+RecipeCard.propTypes = {
+  recipe: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    cuisine: PropTypes.string.isRequired,
+    time: PropTypes.string.isRequired,
+    difficulty: PropTypes.string.isRequired,
+  }).isRequired,
 }
 
 export default RecipeCard
